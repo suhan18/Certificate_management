b292b5a01f7cc795072ea87804f897eba43574d3
suhan18 <134138944+suhan18@users.noreply.github.com>
2024-06-27 21:11:21 +0530

    Update Certificate.js

--- ca_signing/Certificate.js
+++ ca_signing/Certificate.js
@@ -1,12 +1,28 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const certificateSchema = new mongoose.Schema({
-    commonName: { type: String, required: true },
-    ca: { type: mongoose.Schema.Types.ObjectId, ref: 'CertificateAuthority', required: true },
-    certificate: { type: mongoose.Schema.Types.ObjectId, ref: 'ca_files.files' },
-    key: { type: mongoose.Schema.Types.ObjectId, ref: 'ca_files.files' },
-}, { collection: 'certificates' });
+const CertificateSchema = new Schema({
+    commonName: {
+        type: String,
+        required: true,
+    },
+    certificate: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ca_files',
+        required: true,
+    },
+    issuedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CertificateAuthority',
+        required: true,
+    },
+    ca: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CertificateAuthority',
+        required: true,
+    }
+});
 
-const Certificate = mongoose.model('Certificate', certificateSchema);
+const Certificate = mongoose.model('Certificate', CertificateSchema);
 
 module.exports = Certificate;
